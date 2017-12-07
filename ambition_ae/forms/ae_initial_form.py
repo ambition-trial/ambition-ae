1185cd52e755f46e1f1295ac609db927bd47b322
aseokana <aseokana@bhp.org.bw>
2017-12-07 10:01:58 +0200

    import form validator class

--- ambition_ae/forms/ae_initial_form.py
+++ ambition_ae/forms/ae_initial_form.py
@@ -1,10 +1,16 @@
 from django import forms
 
+from ambition_validators import AdverseEventFormValidator
+
+from edc_form_validators import FormValidatorMixin
+
 from ..models import AeInitial
 from .modelform_mixin import ModelformMixin
 
 
-class AeInitialForm(ModelformMixin, forms.ModelForm):
+class AeInitialForm(FormValidatorMixin, ModelformMixin, forms.ModelForm):
+
+    form_validator_cls = AdverseEventFormValidator
 
     action_identifier = forms.CharField(
         label='Action Identifier',
