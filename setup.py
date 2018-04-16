4dcfd75f7108e9978b5e7f3858b5b6bdf6cd4af0
erikvw <ew2789@gmail.com>
2018-04-16 18:03:56 +0200

    setup url

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     author_email='ew2789@gmail.com',
     packages=find_packages(),
     include_package_data=True,
-    url='https://github.com/botswana-harvard/ambition-ae',
+    url='https://github.com/ambition-study/ambition-ae',
     license='GPL license, see LICENSE',
     description='Adverse Event models and handling.',
     long_description=README,
