2ce90977d041021417f64b26d059cb1d1d662464
erikvw <ew2789@gmail.com>
2019-10-08 22:50:41 -0500

    use form base classes from edc-adverse-event

--- ambition_ae/forms/__init__.py
+++ ambition_ae/forms/__init__.py
@@ -1,5 +1,1 @@
-from .ae_followup_form import AeFollowupForm
-from .ae_initial_form import AeInitialForm
-from .ae_tmg_form import AeTmgForm
 from .recurrence_symptom_form import RecurrenceSymptomForm
-from .ae_susar_form import AeSusarForm
